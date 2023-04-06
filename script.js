6877883288bd5a3383ef3e900ae8d44617f1b407
mahsaaabr <mahsa.abrishamii@gmail.com>
2023-04-06 15:59:33 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let password=document.getElementById("password")
+function genPassword(){
+    let chars='1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let passwordLength=12;
+    let password=""
+    for(let i=0;i<=passwordLength;i++){
+       let randomNumber=Math.floor(Math.random()*chars.length)
+       password+=chars.substring(randomNumber,randomNumber+1)
+    }
+    document.getElementById('password').value=password
+}
+function copyPassword(){
+    let copyText=document.getElementById('password')
+    copyText.select()
+    document.execCommand('copy')
+}
